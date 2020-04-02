02cfae15d02646729dabe06703df9ad392d97afa
Kailash Sankar <ksankar@conviva.com>
2020-04-03 01:21:28 +0530

    updated readme

--- README.md
+++ README.md
@@ -14,4 +14,23 @@ Volunteering service managment
 - docker build -t frontline_client:latest .
 - docker run -d --restart=always --name=frontline_client -p 0.0.0.0:3000:80 frontline_client:latest
 
+## alfred config
+
+it's a hacked up cli util to avoid typing large docker commands one by one.
+https://github.com/Kailash-Sankar/alfred
+
+- clone repo
+- setup alfred
+- add config
+
+  frontline_client: {
+  path: `<path_to_repo>`,
+  name: 'frontline_client',
+  bind: '0.0.0.0:3000:80',
+  type: 'standard',
+  },
+
+- alfred --serve frontline_client --dryrun
+- alfred --serve frontline_client
+
 Make sure the frontline-server repo is also running.
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,38 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = merge(common, { 
-    mode: 'production',
-    output: { 
-        path: __dirname + '/dist', 
-        publicPath: '', 
-        filename: 'static/js/[name].[contenthash:5].js',
-        chunkFilename: 'static/js/vendors.[contenthash:5].js', 
-    },
-    // minize js and css files
-    optimization: {
-        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-        splitChunks: {
-            cacheGroups: {
-              common: {
-                name: 'vendors',
-                test: /[\\/]node_modules[\\/]/,
-                chunks: 'all',
-                enforce: true
-              }
-            }
-          }
-    },
-    plugins: [
-        // generate bundle analysis report
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            openAnalyzer: false,
-        }),
-    ],
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "",
+    filename: "static/js/[name].[contenthash:5].js",
+    chunkFilename: "static/js/vendors.[contenthash:5].js"
+  },
+  // minize js and css files
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+      cacheGroups: {
+        common: {
+          name: "vendors",
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    // generate bundle analysis report
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false
+    })
+  ]
 });
