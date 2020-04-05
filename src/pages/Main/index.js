77a6793d381f4a9cd55e3919f39cabd0f45de43a
Kishor Kumar Jagadeesan <Kishor.Jagadeesan@radisys.com>
2020-04-05 09:22:02 +0530

    * Changes to index page to adjust home page look.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { Layout } from "antd";
@@ -30,18 +30,18 @@ function App({ loggedIn, user, volunteerCount }) {
 
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home {...pageProps} />
-        </Route>
+      <Route exact path="/">
+        <Home {...pageProps} />
+      </Route>
       <Layout>
         <SideBar loggedIn={loggedIn} />
         <Layout className={classnames({ [styles.layout]: loggedIn })}>
+          {loggedIn? 
           <TopBar
             loggedIn={loggedIn}
             user={user}
             volunteerCount={volunteerCount}
-          />
+          /> : null}
           <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
             <div className={styles.contentWrapper}>
               <Route path="/volunteer">
@@ -82,7 +82,6 @@ function App({ loggedIn, user, volunteerCount }) {
           </Footer>
         </Layout>
       </Layout>
-      </Switch>
     </Router>
   );
 }
