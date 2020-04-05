99e9be28e16d69cb2b57d905e21f78f1815c5307
Kishor Kumar Jagadeesan <Kishor.Jagadeesan@radisys.com>
2020-04-06 00:12:19 +0530

    Home page header change

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -96,19 +96,18 @@ const HomePage = (props) => {
         <Row>
           <Col span={24} className="layout-header">
             <Row>
-              <Col lg={3} md={3} sm={24} xs={24}>
-                <img src={StateLogo} className="state-logo" />
-              </Col>
-              <Col lg={9} md={9} sm={24} xs={24}>
-                <span className="header-text hashtag-1">
+              <Col lg={6} md={6} sm={8} xs={8} style={{textAlign:'center'}}>
+                <img src={StateLogo} className="state-logo" /><br/>
+                <span className="header-text">
                   #karnatakafightscovid19
                 </span>
               </Col>
-              <Col lg={8} md={8} sm={24} xs={24} className="cm-image-container">
-                <span className="header-text hashtag-2">#donatemadona</span>
+              <Col lg={12} md={12} sm={8} xs={8} className="logo-banner">
+                SANKALPA
               </Col>
-              <Col lg={4} md={4} sm={24} xs={24} className="cm-image-container">
-                <img src={cmIMG} className="cm-logo" />
+              <Col lg={6} md={6} sm={8} xs={8} className="cm-image-container">
+                <img src={cmIMG} className="cm-logo" /><br/>
+                <span className="header-text">#donatemadona</span>
               </Col>
             </Row>
           </Col>
