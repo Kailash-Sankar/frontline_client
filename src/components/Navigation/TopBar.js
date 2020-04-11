9525620c7b93c18458d847423b24e01033643c02
Kailash Sankar <ksankar@conviva.com>
2020-04-11 10:54:54 +0530

    added lazy loading

---
+++ src/components/Navigation/TopBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Layout } from "antd";
+import { Spacer } from "@components/Utils";
+import * as styles from "./index.module.less";
+import StateLogo from "@images/state-logo.png";
+
+import { LoginOutlined, LogoutOutlined } from "@ant-design/icons";
+
+const { Header } = Layout;
+const TopBar = ({ loggedIn, user }) => {
+  return (
+    <Header style={{ background: "#fff", padding: 0 }}>
+      <div className={styles.headFlex}>
+        <div className={styles.logoWrap}>
+          <Link to="/">
+            <img src={StateLogo} className="state-logo" />
+            <span style={{ paddingLeft: "10px", fontWeight: "600" }}>
+              Sankalpa
+            </span>
+          </Link>
+        </div>
+
+        <div className={styles.statusWrap}></div>
+
+        <div className={styles.userWrap}>
+          <Spacer width={20} />
+          {loggedIn ? (
+            <span>
+              <span className={styles.userLabel}>Hello, {user.name}</span>
+              <Spacer width={20} />
+              <Link to="/logout">
+                <LogoutOutlined />
+              </Link>
+            </span>
+          ) : (
+            <Link to="/login">
+              <LoginOutlined />
+            </Link>
+          )}
+        </div>
+      </div>
+    </Header>
+  );
+};
+
+export default TopBar;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { Layout } from "antd";
 //import { HeartOutlined } from "@ant-design/icons";
-
-import { TopBar, SideBar } from "@components/Navigation";
-import PageFooter from "@components/Footer";
+import { Spin } from "antd";
 
 import * as styles from "./index.module.less";
 
@@ -14,16 +13,21 @@ import store from "@store/root";
 import { connecter } from "@store/common";
 import classnames from "classnames";
 
+import PageFooter from "@components/Footer";
+
 import Home from "./Home";
 import Login from "./Login";
 import Logout from "./Logout";
 import LoginRequired from "./LoginRequired";
 
-import VolunteerSignup from "./VolunteerSignup";
-import Kind from "./Kind";
-import Appeal from "./Appeal";
-import Report from "./Report";
-import RequestForHelp from './RequestForHelp';
+const TopBar = lazy(() => import("@components/Navigation/TopBar"));
+const SideBar = lazy(() => import("@components/Navigation/SideBar"));
+
+const VolunteerSignup = lazy(() => import("./VolunteerSignup"));
+const Kind = lazy(() => import("./Kind"));
+const Appeal = lazy(() => import("./Appeal"));
+const Report = lazy(() => import("./Report"));
+const RequestForHelp = lazy(() => import("./RequestForHelp"));
 
 const { Content, Footer } = Layout;
 
@@ -32,60 +36,70 @@ function App({ loggedIn, user, volunteerCount }) {
 
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home {...pageProps} />
-        </Route>
-        <Route>
-          <Layout>
-            <SideBar loggedIn={loggedIn} />
-            <Layout className={classnames({ [styles.layout]: loggedIn })}>
+      <Suspense
+        fallback={
+          <div className={styles.spinner}>
+            <Spin />
+          </div>
+        }
+      >
+        <Switch>
+          <Route exact path="/">
+            <Home {...pageProps} />
+          </Route>
+          <Route>
+            <Layout>
+              <SideBar loggedIn={loggedIn} />
+
+              <Layout className={classnames({ [styles.layout]: loggedIn })}>
                 <TopBar
                   loggedIn={loggedIn}
                   user={user}
                   volunteerCount={volunteerCount}
                 />
-              <Content className={styles.content}>
-                <div className={styles.contentWrapper}>
-                  <Route path="/volunteer">
-                    <VolunteerSignup {...pageProps} />
-                  </Route>
 
-                  <Route exact path="/kind">
-                    <Kind {...pageProps} />
-                  </Route>
+                <Content className={styles.content}>
+                  <div className={styles.contentWrapper}>
+                    <Route path="/volunteer">
+                      <VolunteerSignup {...pageProps} />
+                    </Route>
 
-                  <Route exact path="/appeal">
-                    <LoginRequired loggedIn={loggedIn}>
-                      <Appeal {...pageProps} />
-                    </LoginRequired>
-                  </Route>
+                    <Route exact path="/kind">
+                      <Kind {...pageProps} />
+                    </Route>
 
-                  <Route path="/report">
-                    <LoginRequired loggedIn={loggedIn}>
-                      <Report {...pageProps} />
-                    </LoginRequired>
-                  </Route>
+                    <Route exact path="/appeal">
+                      <LoginRequired loggedIn={loggedIn}>
+                        <Appeal {...pageProps} />
+                      </LoginRequired>
+                    </Route>
 
-                  <Route path="/request">
-                    <RequestForHelp {...pageProps} />
-                  </Route>
+                    <Route path="/report">
+                      <LoginRequired loggedIn={loggedIn}>
+                        <Report {...pageProps} />
+                      </LoginRequired>
+                    </Route>
 
-                  <Route path="/login">
-                    <Login {...pageProps} />
-                  </Route>
-                  <Route path="/logout">
-                    <Logout {...pageProps} />
-                  </Route>
-                </div>
-              </Content>
-              <Footer style={{ textAlign: "center" }}>
-                <PageFooter />
-              </Footer>
+                    <Route path="/request">
+                      <RequestForHelp {...pageProps} />
+                    </Route>
+
+                    <Route path="/login">
+                      <Login {...pageProps} />
+                    </Route>
+                    <Route path="/logout">
+                      <Logout {...pageProps} />
+                    </Route>
+                  </div>
+                </Content>
+                <Footer style={{ textAlign: "center" }}>
+                  <PageFooter />
+                </Footer>
+              </Layout>
             </Layout>
-          </Layout>
-        </Route>
-      </Switch>
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
