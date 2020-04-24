05024850a90fda1600f31633d33572b878d3c49e
rmagadiranga <raghunandanamr@outlook.com>
2020-04-24 22:57:20 +0530

    initial commit of internalization and localization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(
+<I18nextProvider i18n={i18n}>
+    <App/> </I18nextProvider>, document.getElementById("app"));
