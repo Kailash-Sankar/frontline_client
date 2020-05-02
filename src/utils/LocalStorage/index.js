33e16b62d2519ce0c7571f98e741ffbac60c906f
Kailash Sankar <ksankar@conviva.com>
2020-05-02 19:09:43 +0530

    cleanup

--- src/utils/LocalStorage/index.js
+++ src/utils/LocalStorage/index.js
@@ -1,7 +1,7 @@
 import { authStoreKey, userStoreKey } from "@utils/constants";
 import StoreWrap from "store-wrap";
 
-// wrapper for storage
+// generate store handles
 const authStorage = StoreWrap.generateHandle("localStorage", authStoreKey);
 const userStorage = StoreWrap.generateHandle("localStorage", userStoreKey);
 
