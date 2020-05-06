fd4390a39b69262a66eed4ff246834789006ac0c
rmagadiranga <raghunandanamr@outlook.com>
2020-05-06 12:38:43 +0530

    review comments fixed

--- src/components/HomeLayout/Helpline.js
+++ src/components/HomeLayout/Helpline.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Modal, Row, Col, Button } from "antd";
-
+import { CarOutlined } from "@ant-design/icons";
+import { travelLinks } from "@utils/constants";
 // import { WomanOutlined } from "@ant-design/icons";
 // import { Spacer } from "@components/Utils";
 // import { helplineLinks } from "@utils/constants";
@@ -15,74 +16,11 @@ const TravelAssistanceModal = ({ showModal, setShowModal }) => (
   >
     <div style={{ textAlign: "left" }}>
       <div className="modal-txt">
-        <p><a href="https://mapit.gov.in/covid-19" target="_new">
-          <Button type="link">Madhya Pradesh</Button>
-        </a></p>
-        <p><a href="https://www.digitalgujarat.gov.in/loginapp/CitizenLogin.aspx" target="_new">
-          <Button type="link">Gujarat </Button>
-        </a></p>
-        <p><a href="https://covidhelp.punjab.gov.in" target="_new">
-          <Button type="link"> Punjab </Button>
-        </a></p>
-        <p><a href="https://covid19.mhpolice.in" target="_new">
-          <Button type="link">Maharashtra </Button>
-        </a></p>
-        <p><a href="https://emitraapp.rajasthan.gov.in/emitraApps/covid19MigrantRegistrationService" target="_new">
-          <Button type="link">Rajasthan  </Button>
-        </a></p>
-        <p><a href="http://covidepass.hp.gov.in/" target="_new">
-          <Button type="link">Himachal Pradesh  </Button>
-        </a></p>
-        <p><a href="http://tnepass.tnega.gov.in" target="_new">
-          <Button type="link">Tamil Nadu</Button>
-        </a></p>
-        <p><a href="https://edisha.gov.in/eForms/MigrantService" target="_new">
-          <Button type="link">Haryana </Button>
-        </a></p>
-        <p><a href="https://sevasindhu.karnataka.gov.in/Sevasindhu/English" target="_new">
-          <Button type="link">Karnataka </Button>
-        </a></p>
-        <p><a href="http://dsclservices.org.in/uttarakhand-migrant-registration.php" target="_new">
-          <Button type="link">Uttarakhand </Button>
-        </a></p>
-        <p><a href="https://covid19regd.odisha.gov.in/" target="_new">
-          <Button type="link">Odisha </Button>
-        </a></p>
-        <p><a href="goaonline.gov.in " target="_new">
-          <Button type="link">Goa </Button>
-        </a></p>
-        <p><a href="https://covid19jagratha.kerala.nic.in/" target="_new">
-          <Button type="link">Kerala </Button>
-        </a></p>
-        <p><a href="serviceonline.bihar.gov.in" target="_new">
-          <Button type="link">Bihar </Button>
-        </a></p>
-        <p><a href=" jharkhandpravasi.in" target="_new">
-          <Button type="link">Jharkhand</Button>
-        </a></p>
-        <p><a href="http://cglabour.nic.in/covid19MigrantRegistrationService.aspx" target="_new">
-          <Button type="link"> Chhattisgarh</Button>
-        </a></p>
-        <p><a href="http://spandana1.ap.gov.in/Registration/onlineRegistration" target="_new">
-          <Button type="link">Andhra Pradesh </Button>
-        </a></p>
-        <p><a href="Jansunwai.up.nic.in" target="_new">
-          <Button type="link">Uttar Pradesh </Button>
-        </a></p>
-
-
-        {/* <p> Helpline numbers are available in the links below, </p>
-        <span>
-          <a href={helplineLinks.kannada} target="_new">
-            <Button type="primary">ಕನ್ನಡ</Button>
-          </a>
-        </span>
-        <Spacer width={10} />
-        <span>
-          <a href={helplineLinks.english} target="_new">
-            <Button type="primary">English</Button>
-          </a>
-        </span> */}
+        {travelLinks.map((travel, index) => (
+          <p key={index}><a href={travel.link} target="_new" >
+            <Button type="link">{travel.label} </Button>
+          </a></p>
+        ))}
       </div>
     </div >
   </Modal >
@@ -145,7 +83,7 @@ const Helpline = () => {
           onClick={() => setShowModal(true)}
         >
           <Button className="orange-btn">
-            Travel Assistance
+            <CarOutlined />Travel Assistance
         </Button>
         </div>
       </Col>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -21,3 +21,25 @@ export const helplineLinks = {
   kannada: "https://drive.google.com/open?id=1vBTp1pG92sjgv6U6LIHbCu7rC3R-b648",
   english: "https://drive.google.com/open?id=1Mv0CuqmqV5jBv2sX2utuqXfPHIZ01_V_",
 };
+
+export const travelLinks = [
+  { link: "https://mapit.gov.in/covid-19", label: "Madhya Pradesh" },
+  { link: "https://www.digitalgujarat.gov.in/loginapp/CitizenLogin.aspx", label: "Gujarat" },
+  { link: "https://covidhelp.punjab.gov.in", label: "Punjab" },
+  { link: "https://covid19.mhpolice.in", label: "Maharashtra" },
+  { link: "https://emitraapp.rajasthan.gov.in/emitraApps/covid19MigrantRegistrationService", label: "Rajasthan" },
+  { link: "http://covidepass.hp.gov.in/", label: "Himachal Pradesh" },
+  { link: "http://tnepass.tnega.gov.in", label: "Tamil Nadu" },
+  { link: "https://edisha.gov.in/eForms/MigrantService", label: "Haryana" },
+  { link: "https://sevasindhu.karnataka.gov.in/Sevasindhu/English", label: "Karnataka" },
+  { link: "http://dsclservices.org.in/uttarakhand-migrant-registration.php", label: "Uttarakhand" },
+  { link: "https://covid19regd.odisha.gov.in/", label: "Odisha" },
+  { link: "goaonline.gov.in", label: "Goa " },
+  { link: "https://covid19jagratha.kerala.nic.in/", label: "Kerala" },
+  { link: "serviceonline.bihar.gov.in", label: "Bihar" },
+  { link: "jharkhandpravasi.in", label: "Jharkhand" },
+  { link: "http://cglabour.nic.in/covid19MigrantRegistrationService.aspx", label: "Chhattisgarh" },
+  { link: "http://spandana1.ap.gov.in/Registration/onlineRegistration", label: "Andhra Pradesh" },
+  { link: "Jansunwai.up.nic.in", label: "Uttar Pradesh" },
+
+]
