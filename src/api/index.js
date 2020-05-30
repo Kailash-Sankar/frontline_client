cfc119b43fde5fa7bee8c80edfaee03786d19d40
Kailash Sankar <Kailash.Sankar@outlook.com>
2020-05-30 18:12:51 +0530

    websocket setup and demo (#143)
    * websocket demo and hook
    
    * cleanup

--- src/api/index.js
+++ src/api/index.js
@@ -109,6 +109,12 @@ async function authCheck() {
   return res;
 }
 
+// get websocket token
+async function getWebsocketToken() {
+  const res = await server.get(`/auth/token/ws`);
+  return res;
+}
+
 // -- home page --
 async function getHomePageData(url) {
   const res = await server.get(url);
@@ -142,6 +148,7 @@ export default {
   getVolunteerCount,
   login,
   authCheck,
+  getWebsocketToken,
   getHomePageData,
   saveAppealForm,
   searchAppeals,
